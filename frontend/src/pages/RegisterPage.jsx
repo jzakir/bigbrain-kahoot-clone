35b598d32780ee5e1bde292666b14ffa569d65da
Juzer Zakir <jzakir@outlook.com>
2023-04-07 17:07:34 +1000

    added basic loading state to login and register

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -11,27 +11,31 @@ import axios from '../axios';
 
 export default function RegisterPage () {
   const navigate = useNavigate();
+
+  const [loading, setLoading] = React.useState(false);
+
   const [name, setName] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setLoading(true);
 
     axios
       .post('/admin/auth/register', {
-        email: email,
-        password: password,
-        name: name
+        email,
+        password,
+        name
       })
-      .then(data => {
-        console.log(data);
+      .then(response => {
+        console.log(response.data.token);
       })
       .catch(err => {
         console.log(err);
       })
       .finally(() => {
-        console.log('add loading state here');
+        setLoading(false);
       })
   }
 
@@ -50,53 +54,61 @@ export default function RegisterPage () {
           }}
         >
           <Typography variant="h4" sx={{ mb: 2 }}>Register</Typography>
-          <Box component='form' noValidate onSubmit={handleSubmit}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  autoComplete="name"
-                  name="name"
-                  required
-                  fullWidth
-                  id="name"
-                  label="Name"
-                  onChange={(e) => setName(e.target.value)}
-                  autoFocus
-                />
+          {loading
+            ? (
+            <div>
+              <Typography variant='h6'>Loading...</Typography>
+            </div>
+              )
+            : (
+            <Box component='form' noValidate onSubmit={handleSubmit}>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    autoComplete="name"
+                    name="name"
+                    required
+                    fullWidth
+                    id="name"
+                    label="Name"
+                    onChange={(e) => setName(e.target.value)}
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type='submit'
-              variant="contained"
-              fullWidth
-              sx={{ mt: 3, mb: 2 }}
-            >
-            Register
-            </Button>
-          </Box>
+              <Button
+                type='submit'
+                variant="contained"
+                fullWidth
+                sx={{ mt: 3, mb: 2 }}
+              >
+              Register
+              </Button>
+            </Box>
+              )}
           <Typography variant="h6">
             Already have an account?
             <Link to="/login">Sign In</Link>
