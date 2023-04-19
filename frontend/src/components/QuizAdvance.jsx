4f17cda4f3373d96daadb99f051d9b42d01aab4e
Juzer Zakir <jzakir@outlook.com>
2023-04-19 15:23:03 +1000

    added a timer for admin side

--- frontend/src/components/QuizAdvance.jsx
+++ frontend/src/components/QuizAdvance.jsx
@@ -5,9 +5,18 @@ import GradientButton from './GradientButton';
 import WhiteBox from './WhiteBox';
 
 export default function QuizAdvance (props) {
+  const [seconds, setSeconds] = React.useState(0);
+
+  React.useEffect(() => {
+    setSeconds(props.currPos > -1 && props.currPos < props.questions.length ? props.questions[props.currPos].timeLimit : 0);
+    const interval = window.setInterval(() => {
+      setSeconds(s => s - 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [props.currPos])
+
   const questionString = () => {
     const size = props.questions.length;
-    console.log(props.currPos);
     if (props.currPos < size) {
       if (props.currPos === -1) {
         return 'The game hasn\'t started yet.';
@@ -17,15 +26,15 @@ export default function QuizAdvance (props) {
     }
     return '';
   }
-  console.log(props.questions);
 
   return (
     <Container maxWidth="lg" sx={{ mt: 5 }}>
       <Typography variant='h3' sx={{ color: 'white', mt: 20, mb: 4 }}>Manage Game</Typography>
       <WhiteBox sx={{ height: '500px' }}>
         <Typography variant='h2' sx={{ fontWeight: 'bold', color: 'rgb(248,130,135)' }}>{props.quizName}</Typography>
-        <Box sx={{ display: 'flex', width: '100%', height: '200px', backgroundColor: 'lightgrey', mt: 4, justifyContent: 'center', borderRadius: '10px' }}>
-          <Typography variant='h6' sx={{ mt: 2 }}> { questionString() }</Typography>
+        <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%', height: '200px', backgroundColor: 'lightgrey', mt: 4, alignItems: 'center', borderRadius: '10px' }}>
+          <Typography variant='h4' sx={{ mt: 2 }}> { questionString() }</Typography>
+          <Typography variant='h5' sx={{ mt: 4 }}>Time Left: {seconds >= 0 ? seconds : '0'}</Typography>
         </Box>
         <Box sx={{ display: 'flex', mt: 10 }}>
           <PrimaryButton onClick={ props.handleStop } sx={{ ml: 4 }}>
